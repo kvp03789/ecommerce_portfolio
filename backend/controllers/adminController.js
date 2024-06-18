d5c368a20df0b4a442ffdcae507b5c679e6ac696
Clay Nichols <clay.nichols@gmail.com>
2024-06-18 13:37:59 -0500

    add sql queries to get routes in /admin

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,18 +1,26 @@
 const db = require('../config/database')
+const asyncHandler = require('express-async-handler')
 
-exports.get_all_customers = async(req, res, next) => {
+exports.get_all_customers = asyncHandler(async(req, res, next) => {
     let sql = `SELECT * FROM customers`
     db.query(sql, (err, result)=>{
         if(err) throw err
         res.json({message: 'GET all customers', result})
     })
-    
-}
+})
 
-exports.get_all_products = async(req, res, next) => {
-    res.json({message: 'GET all products'})
-}
+exports.get_all_products = asyncHandler(async(req, res, next) => {
+    let sql = `SELECT * FROM products`
+    db.query(sql, (err, result)=>{
+        if(err) throw err
+        res.json({message: 'GET all products', result})
+    })
+})
 
-exports.get_all_orders = async(req, res, next) => {
-    res.json({message: 'GET all orders'})
-}
+exports.get_all_orders = asyncHandler(async(req, res, next) => {
+    let sql = `SELECT * FROM orders`
+    db.query(sql, (err, result)=>{
+        if(err) throw err
+        res.json({message: 'GET all orders', result})
+    })
+})
